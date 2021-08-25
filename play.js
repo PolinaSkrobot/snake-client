c9fb9c3be639ad48f937f54bc16924d9ea2de4a1
Polina Skrobot <chi.polly.no@gmail.com>
2021-08-25 19:54:57 +0000

    Shorthand syntax added for export/import

--- play.js
+++ play.js
@@ -1,16 +1,3 @@
-const net = require("net");
-
-const connect = function () {
-  const conn = net.createConnection({
-    host: '135.23.223.133',
-    port: 50542
-  });
-  conn.setEncoding("utf8");
-  conn.on("data", (data)=> {
-    console.log(data);
-  });
-  return conn;
-  
-};
+const short = require('./client');
 console.log("Connecting ...");
-connect();
+short.connect();
