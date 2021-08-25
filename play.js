3ce702e08790eabe11cc7fe2743f805c04ed5505
Polina Skrobot <chi.polly.no@gmail.com>
2021-08-25 21:02:09 +0000

    Ctrl+C added

--- play.js
+++ play.js
@@ -1,3 +1,18 @@
 const short = require('./client');
 console.log("Connecting ...");
 short.connect();
+const setupInput = function() {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  const handleUserInput = function(key) {
+    if (key === '\u0003') {
+      process.exit();
+    }
+    //process.stdout.write('.');
+  };
+  stdin.on('data',handleUserInput);
+  stdin.resume();
+  return stdin;
+};
+setupInput();
